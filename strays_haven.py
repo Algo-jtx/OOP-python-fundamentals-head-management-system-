b28f67c64aada7ab56e117203fb2fe56b5b4b554
juanita mumbi <mumbijuanita@gmail.com>
2025-05-15 10:12:01 +0300

    test file

--- strays_haven.py
+++ strays_haven.py
@@ -1,10 +1,24 @@
+# Class initialisation with default __init__
 class Pet:
     def speak(self):
         print("sound made")
-    pass
-    
+        # return ("pet spoke")
+
+Rasmus = Pet()
+Rasmus.name = "Rasmus"
+print(Rasmus.name)
+print(Rasmus.speak())        
+
+
+# A simple class to represent a dog modified __init__    
 class Dog:
-    pass
+    def __init__(self,name,breed,age="N/A"):
+        self.name = name
+        self.breed = breed
+        self.age = age
+    def speak(self):
+        return f"1  "        
+    
 
 class Cat:
     pass
